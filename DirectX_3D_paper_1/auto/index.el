22119a565f7c12a3d9ee233bc84a5811db35597f
taishi-matsumura <taishi0706@gmail.com>
2018-10-23 10:11:33 +0900

    [:up: #3] DirectX(3D)の筆記試験(その1)を追加

---
+++ DirectX_3D_paper_1/auto/index.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "index"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org56aeac4"
+    "sec:orgb7bb1db"
+    "sec:org0dd9955"
+    "sec:orge1ec434"
+    "sec:org7cdda78"
+    "sec:org7a0a377"
+    "sec:org816cb46"
+    "sec:org1c60f83"))
+ :latex)
+
